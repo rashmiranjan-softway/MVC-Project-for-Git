a91ca77d48a80c4e151a667da4ade840c2602ecb
rashmiranjan <rashmiranjan@softwaysolutions.com>
2015-04-08 10:54:44 +0530

    Initial Commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Project_for_Git.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Project_for_Git.Startup))]
+namespace MVC_Project_for_Git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
